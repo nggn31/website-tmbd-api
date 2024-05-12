fc9b126110f6f3e31da970e35a5dff1977bb02e0
nggn31 <159208505+nggn31@users.noreply.github.com>
2024-05-12 13:57:12 -0700

    se agrego el evento del icono de favoritos

--- script-api-series.js
+++ script-api-series.js
@@ -21,68 +21,76 @@ function getTVShows(url) {
     });
 }
 
-function showTVShows(data){
-  const showsContainer = document.getElementById('shows'); // Obtener el contenedor de películas
+function showTVShows(data) {
+  const showsContainer = document.getElementById("shows"); // Obtener el contenedor de películas
 
-  showsContainer.innerHTML = ''; // Limpiar el contenedor de películas antes de agregar nuevas películas
+  showsContainer.innerHTML = ""; // Limpiar el contenedor de películas antes de agregar nuevas películas
 
+  data.forEach((show) => {
+    const { title, poster_path, vote_average, overview } = show;
 
-  data.forEach(show => {
-      const {title, poster_path, vote_average, overview} = show;
+    // Crear un elemento div para la película
+    const showEl = document.createElement("div");
+    showEl.classList.add("box");
 
-      // Crear un elemento div para la película
-      const showEl = document.createElement('div');
-      showEl.classList.add('box');
+    // Crear un elemento imagen para el póster de la película
+    const img = document.createElement("img");
+    img.classList.add("box-img");
+    img.src = `${IMG_URL}${poster_path}`;
+    img.alt = title;
 
-      // Crear un elemento imagen para el póster de la película
-      const img = document.createElement('img');
-      img.classList.add('box-img');
-      img.src = `${IMG_URL}${poster_path}`;
-      img.alt = title;
+    // Crear un elemento icono para el favorito
+    const favIcon = document.createElement("ion-icon");
+    favIcon.classList.add("box-fav");
+    favIcon.setAttribute("name", "heart-outline");
 
-      // Crear un elemento icono para el favorito
-      const favIcon = document.createElement('ion-icon');
-      favIcon.classList.add('box-fav');
-      favIcon.setAttribute('name', 'heart-outline');
+    // Agregar un evento de clic para activar la animación al hacer clic en el icono de favoritos
+    favIcon.addEventListener("click", () => {
+      favIcon.classList.toggle("active");
+      icon.classList.toggle("active");
+    });
 
-      // Crear un elemento div para el contenido de la película
-      const showContent = document.createElement('div');
-      showContent.classList.add('box-content');
+    // Crear un elemento div para el contenido de la película
+    const showContent = document.createElement("div");
+    showContent.classList.add("box-content");
 
-      // Crear un elemento h2 para el título de la película
-      const titleEl = document.createElement('h2');
-      titleEl.textContent = title;
+    // Crear un elemento h2 para el título de la película
+    const titleEl = document.createElement("h2");
+    titleEl.textContent = title;
 
-      // Crear un elemento div para el rating de la película
-      const ratingDiv = document.createElement('div');
-      ratingDiv.classList.add('box-content-rating');
+    // Crear un elemento div para el rating de la película
+    const ratingDiv = document.createElement("div");
+    ratingDiv.classList.add("box-content-rating");
 
-      // Crear y agregar iconos de estrella al rating
-      for (let i = 0; i < Math.floor(vote_average / 2); i++) {
-          const starIcon = document.createElement('ion-icon');
-          starIcon.setAttribute('name', 'star');
-          ratingDiv.appendChild(starIcon);
-      }
+    // Crear y agregar iconos de estrella al rating
+    for (let i = 0; i < Math.floor(vote_average / 2); i++) {
+      const starIcon = document.createElement("ion-icon");
+      starIcon.setAttribute("name", "star");
+      ratingDiv.appendChild(starIcon);
+    }
 
-      // Crear un elemento p para la descripción de la película
-      const overviewEl = document.createElement('p');
-      overviewEl.textContent = overview;
+    // Crear un elemento p para la descripción de la película
+    const overviewEl = document.createElement("p");
+    overviewEl.textContent = overview;
 
-      // Truncar el texto del resumen para mostrar solo una parte de él
-      const maxLength = 90; // Define la longitud máxima del texto del resumen
-      const truncatedOverview = overview.length > maxLength ? overview.substring(0, maxLength) + '...' : overview;
-      overviewEl.textContent = truncatedOverview;
+    // Truncar el texto del resumen para mostrar solo una parte de él
+    const maxLength = 90; // Define la longitud máxima del texto del resumen
+    const truncatedOverview =
+      overview.length > maxLength
+        ? overview.substring(0, maxLength) + "..."
+        : overview;
+    overviewEl.textContent = truncatedOverview;
 
-      // Agregar todos los elementos creados al elemento de película
-      showContent.appendChild(titleEl);
-      showContent.appendChild(ratingDiv);
-      showContent.appendChild(overviewEl);
-      
-      showEl.appendChild(img);
-      showEl.appendChild(favIcon);
-      showEl.appendChild(showContent);
-      
-      showsContainer.appendChild(showEl); // Agregar el elemento de película al contenedor de películas
+    // Agregar todos los elementos creados al elemento de película
+    showContent.appendChild(titleEl);
+    showContent.appendChild(ratingDiv);
+    showContent.appendChild(overviewEl);
+
+    showEl.appendChild(img);
+    showEl.appendChild(favIcon);
+    showEl.appendChild(showContent);
+
+    showsContainer.appendChild(showEl); // Agregar el elemento de película al contenedor de películas
   });
 }
 
