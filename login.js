39fb38bde8a81c35314d65dbf4971961a4b34b94
jenni <jennifer_romero3@hotmail.com>
2024-05-12 22:44:39 -0700

    se agrego js de login

---
+++ login.js
@@ -0,0 +1,27 @@
+// Objeto JSON simulado para almacenar usuarios
+const users = [
+    { username: "jennifer", password: "12345" },
+    { username: "nia", password: "12345" }
+];
+
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector(".login");
+
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+      // Verificar si el usuario y la contraseña coinciden con los almacenados
+        const user = users.find(user => user.username === username && user.password === password);
+
+        if (user) {
+        alert("Inicio de sesión exitoso");
+        // Aquí puedes redirigir al usuario a otra página o realizar alguna acción adicional
+        window.location.href = "landing.html";
+        } else {
+        alert("Nombre de usuario o contraseña incorrectos");
+        }
+    });
+});
